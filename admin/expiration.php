f0dd5843fb405e1ff30399ea48528ef376eb827e
unknown <manlioelnum1@hotmail.com>
2015-11-07 19:24:30 -0600

    Expiration remove

--- admin/expiration.php
+++
@@ -1,8 +0,0 @@
-<?PHP
-//cookies antes de session_start
-$lifetime = 60000;
-session_set_cookie_params($lifetime);
-ini_set("session.cookie_lifetime",$lifetime);
-ini_set("session.gc_maxlifetime",$lifetime);
-ini_set('max_execution_time', $lifetime);
-?>
--- admin/session.php
+++ admin/session.php
@@ -1,5 +1,11 @@
 <?PHP
-require_once('expiration.php');
+//cookies antes de session_start
+$lifetime = 60000;
+session_set_cookie_params($lifetime);
+ini_set("session.cookie_lifetime",$lifetime);
+ini_set("session.gc_maxlifetime",$lifetime);
+ini_set('max_execution_time', $lifetime);
+
 session_start();  
 
 if($_SESSION['priv']){
