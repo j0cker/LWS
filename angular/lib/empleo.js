ded58f206d44ddaac0d383e8191b3a8fa56dd666
Jocker <manlioelnum1@hotmail.com>
2016-02-17 14:00:55 -0600

    Ng-repeat empleos

--- angular/lib/empleo.js
+++ angular/lib/empleo.js
@@ -3,6 +3,7 @@ app.controller('empleoCtrl', function($scope, evt){
   evt.getCategorias().then(function (response) {
     console.log("Get Categorias");
     console.log(response.data);
+    $scope.todasCategorias = response.data;
   }, function (response) {
     /*ERROR*/
     alert("ERROR");
@@ -11,6 +12,7 @@ app.controller('empleoCtrl', function($scope, evt){
   evt.getVacantes(1).then(function (response) {
     console.log("Get Vacantes");
     console.log(response.data);
+    $scope.todasVacantes = response.data;
   }, function (response) {
     /*ERROR*/
     alert("ERROR");
