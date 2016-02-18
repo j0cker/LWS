458673ea8aebfda36502fcb2aea2b511467f38a9
Jocker <manlioelnum1@hotmail.com>
2016-02-17 22:53:41 -0600

    Ver más Push Json

--- angular/lib/empleo.js
+++ angular/lib/empleo.js
@@ -1,5 +1,7 @@
 app.controller('empleoCtrl', function($scope, evt){
     
+  var hojaGlobal = 1;
+    
   evt.getCategorias().then(function (response) {
     console.log("Get Categorias");
     console.log(response.data);
@@ -9,7 +11,7 @@ app.controller('empleoCtrl', function($scope, evt){
     alert("ERROR");
   });
   
-  evt.getVacantes(1).then(function (response) {
+  evt.getVacantes(hojaGlobal).then(function (response) {
     console.log("Get Vacantes");
     console.log(response.data);
     $scope.todasVacantes = response.data;
@@ -17,5 +19,31 @@ app.controller('empleoCtrl', function($scope, evt){
     /*ERROR*/
     alert("ERROR");
   });
+  
+  $scope.incrementarHoja = function(){
+    console.log("Incrementar Hoja");
+    hojaGlobal = hojaGlobal +1;
+    evt.getVacantes(hojaGlobal).then(function (response) {
+        console.log("Get Vacantes");
+        console.log(response.data);
+        if(response.data.true=="false"){
+            alert("No hay más empleos");
+        } else {
+          for(var sd213=0; sd213<response.data.categoria.length; sd213++){
+            $scope.todasVacantes.categoria.push(response.data.categoria[sd213]);
+            $scope.todasVacantes.estado.push(response.data.estado[sd213]);
+            $scope.todasVacantes.fecha.push(response.data.fecha[sd213]);
+            $scope.todasVacantes.id.push(response.data.id[sd213]);
+            $scope.todasVacantes.nombreEmpresa.push(response.data.nombreEmpresa[sd213]);
+            $scope.todasVacantes.tipoTiempo.push(response.data.tipoTiempo[sd213]);
+          }
+          //$scope.todasVacantes = JSON.stringify($scope.todasVacantes); 
+          console.log($scope.todasVacantes);
+        }
+    }, function (response) {
+        /*ERROR*/
+        alert("ERROR");
+    });
+  };
   
 });
